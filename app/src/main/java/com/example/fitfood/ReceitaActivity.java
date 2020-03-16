d80ee7ee1b8abfe318ed8ff2c1003737e863cacc
RODRIGO SARMENTO XAVIER <rodrigosarmentoxx@gmail.com>
2020-03-15 23:56:16 -0300

    GridView added in AlmocoFragment

--- app/src/main/java/com/example/fitfood/ReceitaActivity.java
+++ app/src/main/java/com/example/fitfood/ReceitaActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.MenuItem;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
