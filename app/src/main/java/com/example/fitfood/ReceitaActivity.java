a5dc33acdfaeea5b5909414aa0e409a8623972f5
RODRIGO SARMENTO XAVIER <rodrigosarmentoxx@gmail.com>
2020-03-15 23:54:49 -0300

    GridView added in AlmocoFragment

--- app/src/main/java/com/example/fitfood/ReceitaActivity.java
+++ app/src/main/java/com/example/fitfood/ReceitaActivity.java
@@ -4,57 +4,22 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.GridView;
-import android.widget.ImageView;
-import android.widget.TextView;
-
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-
-import com.example.fitfood.R;
 import com.google.android.material.navigation.NavigationView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class ReceitaActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
 
-    // VARIÁVEIS PARA O GRIDVIEW
-    int images[] = {R.drawable.batata_doce, R.drawable.brownie_banana, R.drawable.crepioca_requeijao, R.drawable.panqueca_banana};
-    String names[] = {"Batata Doce", "Brownie de Banana", "Crepioca de Requeijão", "Panqueca de Banana"};
-    String desc[] = {"batata doce", "brownie", "crepioca", "panqueca"};
-    GridView gridView;
-
-    List<ItemsGridView> itemsList = new ArrayList<>();
-    CustomAdapter customAdapter;
-    // FIM VARIÁVEIS PARA O GRIDVIEW
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activty_receita);
-
-        // PARADAS DO GRIDVIEW
-        gridView = findViewById(R.id.grid_view_receita_pre_treino);
-
-        for (int i = 0; i < names.length; i++) {
-            ItemsGridView itemsGridView = new ItemsGridView(names[i], desc[i], images[i]);
-            itemsList.add(itemsGridView);
-        }
-        customAdapter = new CustomAdapter(itemsList, this);
-        gridView.setAdapter(customAdapter);
-        // FIM DAS PARADAS DO GRIDVIEW
-
 
         //Set Action Bar
         Toolbar toolbar = findViewById(R.id.toolbar_receita);
@@ -69,7 +34,7 @@ public class ReceitaActivity extends AppCompatActivity implements NavigationView
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         toggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.colorWhite));
         drawer.addDrawerListener(toggle);
-        drawer.openDrawer(Gravity.START);
+        drawer.openDrawer(GravityCompat.START);
         toggle.syncState();//rotate de hamburger icon
 
         NavigationView navigationView = findViewById(R.id.nav_receita);//Looking the Navigation view we created in activity_receita
@@ -132,47 +97,4 @@ public class ReceitaActivity extends AppCompatActivity implements NavigationView
         }
     }
 
-    public class CustomAdapter extends BaseAdapter {
-
-        private List<ItemsGridView> itemsGridViews;
-        private List<ItemsGridView> itemsGridViewsFiltered;
-        private Context context;
-
-        public CustomAdapter(List<ItemsGridView> itemsGridViews, Context context) {
-            this.itemsGridViews = itemsGridViews;
-            this.itemsGridViewsFiltered = itemsGridViews;
-            this.context = context;
-        }
-
-        @Override
-        public int getCount() {
-            return itemsGridViewsFiltered.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return null;
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            LayoutInflater inflater = LayoutInflater.from(context);
-
-            View view = inflater.inflate(R.layout.item_grid, null);
-            ImageView imageView = view.findViewById(R.id.receita_imageView);
-            TextView textView_Name = view.findViewById(R.id.receita_textView_Name);
-            TextView textView_Desc = view.findViewById(R.id.receita_textView_Desc);
-
-            imageView.setImageResource(itemsGridViewsFiltered.get(position).getImage());
-            textView_Name.setText(itemsGridViewsFiltered.get(position).getName());
-            textView_Desc.setText(itemsGridViewsFiltered.get(position).getDesc());
-
-            return view;
-        }
-    }
 }
--- app/src/main/java/com/example/fitfood/ReceitaFragmentAlmoco.java
+++ app/src/main/java/com/example/fitfood/ReceitaFragmentAlmoco.java
@@ -1,21 +1,96 @@
 package com.example.fitfood;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SuppressWarnings("WeakerAccess")
 public class ReceitaFragmentAlmoco extends Fragment {
 
+    // VARIÁVEIS PARA O GRIDVIEW
+    int[] images = {R.drawable.batata_doce, R.drawable.brownie_banana, R.drawable.crepioca_requeijao, R.drawable.panqueca_banana};
+    String[] names = {"Batata Doce", "Brownie de Banana", "Crepioca de Requeijão", "Panqueca de Banana"};
+    String[] desc = {"batata doce", "brownie", "crepioca", "panqueca"};
+    GridView gridView;
+    List<ItemsGridView> itemsList = new ArrayList<>();
+    CustomAdapter customAdapter;
+
+    // FIM VARIÁVEIS PARA O GRIDVIEW
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_receita_almoco,container, false);
+
+        View view = inflater.inflate(R.layout.fragment_receita_almoco, container, false);
+
+        // PARADAS DO GRIDVIEW
+        gridView = view.findViewById(R.id.grid_view_receita_almoco);
+
+        for (int i = 0; i < names.length; i++) {
+            ItemsGridView itemsGridView = new ItemsGridView(names[i], desc[i], images[i]);
+            itemsList.add(itemsGridView);
+        }
+        customAdapter = new CustomAdapter(itemsList, getContext());
+        gridView.setAdapter(customAdapter);
+        // FIM DAS PARADAS DO GRIDVIEW
+
+        return view;
+
+    }
+
+    public class CustomAdapter extends BaseAdapter {
+        private List<ItemsGridView> itemsGridViews;
+        private List<ItemsGridView> itemsGridViewsFiltered;
+        private Context context;
+
+        public CustomAdapter(List<ItemsGridView> itemsGridViews, Context context) {
+            this.itemsGridViews = itemsGridViews;
+            this.itemsGridViewsFiltered = itemsGridViews;
+            this.context = getContext();
+        }
+
+        @Override
+        public int getCount() {
+            return itemsGridViewsFiltered.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+
+            View view = inflater.inflate(R.layout.item_grid, null);
+            ImageView imageView = view.findViewById(R.id.receita_imageView);
+            TextView textView_Name = view.findViewById(R.id.receita_textView_Name);
+            TextView textView_Desc = view.findViewById(R.id.receita_textView_Desc);
+
+            imageView.setImageResource(itemsGridViewsFiltered.get(position).getImage());
+            textView_Name.setText(itemsGridViewsFiltered.get(position).getName());
+            textView_Desc.setText(itemsGridViewsFiltered.get(position).getDesc());
+
+            return view;
+        }
     }
 }
